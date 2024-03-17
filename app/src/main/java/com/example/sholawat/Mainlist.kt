b15222ed9e1e9680fefc1011deebfc6c2fed557b
DheaAmandaFajriani <dheamandaa203@gmail.com>
2024-03-17 18:05:23 +0700

    first commit

---
+++ app/src/main/java/com/example/sholawat/Mainlist.kt
@@ -0,0 +1,11 @@
+package com.example.sholawat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mainlist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainlist)
+    }
+}
